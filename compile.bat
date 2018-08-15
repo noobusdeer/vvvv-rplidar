c5db8f467d47fc31cbf36c2c89244012dec253e5
Gleb Storozhik <hello@noobusdeer.com>
2018-08-15 14:50:44 +0300

    rework compile pipeline

---
+++ compile.bat
@@ -0,0 +1,5 @@
+cmake -G "Visual Studio 15 2017 Win64" -Blib-rplidar\build -Hlib-rplidar
+MSbuild lib-rplidar\build\lib-rplidar.sln /m /p:Configuration=Debug;Platform=x64
+XCOPY lib-rplidar\build\Debug\lib-rplidar.dll out /m /e /y
+MSbuild vvvv-rplidar\vvvv-rplidar.sln /m /p:Configuration=Debug;Platform="Any CPU"
+XCOPY vvvv-rplidar\build\vvvv-rplidar.dll out /m /e /y
